c9af79f612ee015698e6f366eafce84c6ab023da
Frozen <1884084+xrdavies@users.noreply.github.com>
2024-08-25 03:22:41 +0800

    ci: disable sourcemap for production, enable for development

--- scripts/start.js
+++ scripts/start.js
@@ -3,6 +3,7 @@
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
+process.env.GENERATE_SOURCEMAP = 'true'; // enable sourcemaps for development build.
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
